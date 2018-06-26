d47cd3d48c141fceb7872e2af84a61fb0fb97f2c
FuyukaiiDesuu <razorsharpbenjo@gmail.com>
2018-06-26 16:00:47 +0800

    heey

--- MainSystem/MainSystem/Stockin-out.Designer.cs
+++ MainSystem/MainSystem/Stockin-out.Designer.cs
@@ -34,12 +34,15 @@
             this.btnExit = new System.Windows.Forms.Button();
             this.lblSinoutRecord = new System.Windows.Forms.Label();
             this.bntView = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.test = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnNewEntry
             // 
             this.btnNewEntry.Location = new System.Drawing.Point(31, 390);
-            this.btnNewEntry.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnNewEntry.Margin = new System.Windows.Forms.Padding(4);
             this.btnNewEntry.Name = "btnNewEntry";
             this.btnNewEntry.Size = new System.Drawing.Size(100, 28);
             this.btnNewEntry.TabIndex = 1;
@@ -49,7 +52,7 @@
             // btnin
             // 
             this.btnin.Location = new System.Drawing.Point(169, 390);
-            this.btnin.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnin.Margin = new System.Windows.Forms.Padding(4);
             this.btnin.Name = "btnin";
             this.btnin.Size = new System.Drawing.Size(100, 28);
             this.btnin.TabIndex = 2;
@@ -59,7 +62,7 @@
             // btnout
             // 
             this.btnout.Location = new System.Drawing.Point(304, 390);
-            this.btnout.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnout.Margin = new System.Windows.Forms.Padding(4);
             this.btnout.Name = "btnout";
             this.btnout.Size = new System.Drawing.Size(100, 28);
             this.btnout.TabIndex = 3;
@@ -69,7 +72,7 @@
             // btnExit
             // 
             this.btnExit.Location = new System.Drawing.Point(596, 390);
-            this.btnExit.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(4);
             this.btnExit.Name = "btnExit";
             this.btnExit.Size = new System.Drawing.Size(100, 28);
             this.btnExit.TabIndex = 4;
@@ -90,27 +93,45 @@
             // bntView
             // 
             this.bntView.Location = new System.Drawing.Point(453, 390);
-            this.bntView.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.bntView.Margin = new System.Windows.Forms.Padding(4);
             this.bntView.Name = "bntView";
             this.bntView.Size = new System.Drawing.Size(100, 28);
             this.bntView.TabIndex = 6;
             this.bntView.Text = "View";
             this.bntView.UseVisualStyleBackColor = true;
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.test});
+            this.dataGridView1.Location = new System.Drawing.Point(31, 165);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(821, 218);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // test
+            // 
+            this.test.HeaderText = "test";
+            this.test.Name = "test";
+            // 
             // Stockin_out
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(895, 554);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.bntView);
             this.Controls.Add(this.lblSinoutRecord);
             this.Controls.Add(this.btnExit);
             this.Controls.Add(this.btnout);
             this.Controls.Add(this.btnin);
             this.Controls.Add(this.btnNewEntry);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Stockin_out";
             this.Text = "Stockin_out";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -123,5 +144,7 @@
         private System.Windows.Forms.Button btnExit;
         private System.Windows.Forms.Label lblSinoutRecord;
         private System.Windows.Forms.Button bntView;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn test;
     }
 }
